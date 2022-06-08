5f3941ae563ac64d6b8fc3dcabf2ce68bb8290fa
juliagamaol <gamaj235@gmail.com>
2022-06-07 23:04:50 -0300

    chore: changed folder name

---
+++ 1-recipes/recipes/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(req):
+    return render(req, 'recipes/pages/home.html', {
+        'name': 'Julia Gama',
+    })
