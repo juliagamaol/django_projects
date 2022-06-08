619148835fd3dfd800cd7a0dbd3aa16eb880e734
Julia Gama de Oliveira <86054348+juliagamaol@users.noreply.github.com>
2022-06-07 23:05:45 -0300

    Delete recipes directory

--- recipes/recipes/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-def home(req):
-    return render(req, 'recipes/pages/home.html', {
-        'name': 'Julia Gama',
-    })
